f2822ecce2d38257156b2415f34ba863d63dd6d8
Juhanda Azril Kamal <cukjuhan@gmail.com>
2024-10-18 20:17:28 +0800

    fix geografis

--- src/Section/Geografis.tsx
+++ src/Section/Geografis.tsx
@@ -7,14 +7,14 @@ const Geografis: React.FC = () => {
     <section className="flex flex-col md:flex-row items-center justify-between  min-h-screen mx-8 relative">
       <div className="absolute inset-0 opacity-30 pointer-events-none"></div>
 
-      <div className="md:absolute 2xl:top-24 2xl:right-[5%] md:top-24 md:left-[25%] w-full md:w-auto text-center md:text-center 2xl:text-right mb-6 md:mb-0">
+      <div className="md:absolute lg:top-24 lg:right-[5%] md:top-24 md:left-[25%] w-full md:w-auto text-center md:text-center lg:text-right mb-6 md:mb-0">
         <p className="text-green-600 text-lg">Peta Desa Genggelang</p>
         <h1 className="text-white text-[2rem] md:text-[2.5rem] font-bold mb-2">
           Letak Geografis
         </h1>
       </div>
 
-      <div className="relative z-0 w-full 2xl:w-1/2 max-w-4xl mb-4 md:mb-0 md:absolute 2xl:absolute md:left-[10%] md:top-[20%] 2xl:left-[10%] 2xl:top-[15%]">
+      <div className="relative z-0 w-full lg:w-1/2 max-w-4xl mb-4 md:mb-0 md:absolute lg:absolute md:left-[10%] md:top-[20%] lg:left-[10%] lg:top-[15%]">
         <img
           src={letakGeografis}
           alt="Peta Desa Genggelang"
@@ -30,20 +30,23 @@ const Geografis: React.FC = () => {
         </PinContainer>
       </div>
 
-      <h1 className="absolute top-[13%] left-[10%] md:top-[66%] 2xl:left-[1%] 2xl:top-[66%] text-white/70 transition-all duration-300 hover:text-white italic pl-3 pt-2 text-[10px] md:text-sm mb-4">
+      <h1 className="absolute top-[13%] left-[10%] md:top-[66%] sm:hidden lg:left-[6%] lg:top-[66%] text-white/70 transition-all duration-300 hover:text-white italic pl-3 pt-2 text-[10px] md:text-sm mb-4">
         Kabupaten Lombok Utara, Nusa Tenggara Barat
       </h1>
 
 
       <div className="bg-[#141414] backdrop-blur-md p-4 text-white/80 rounded-xl shadow-lg md:border border-white/30 w-full md:w-auto md:max-w-md md:mb-0 md:absolute md:left-[63%] md:top-[27%]">
         <p className="text-sm md:text-base mb-4">
-          - Sebelah Utara: Desa Genggelang berbatasan langsung dengan Laut Jawa,
-          memberikan akses ke kawasan pantai. <br />
-          - Sebelah Timur: Berbatasan dengan
-          Desa Gondang, yang juga terletak di Kabupaten Lombok Utara. <br />
-          - Sebelah Selatan: Desa ini berbatasan dengan Desa Rempek, salah satu desa
-          tetangga di bagian selatan. <br />
-          - Sebelah Barat: Berbatasan dengan Desa
+          Sebelah Utara: Desa Genggelang berbatasan langsung dengan Laut Jawa,
+          memberikan akses ke kawasan pantai. <br /><br />
+
+          Sebelah Timur: Berbatasan dengan
+          Desa Gondang, yang juga terletak di Kabupaten Lombok Utara.  <br /><br />
+
+          Sebelah Selatan: Desa ini berbatasan dengan Desa Rempek, salah satu desa
+          tetangga di bagian selatan.  <br /><br />
+
+          Sebelah Barat: Berbatasan dengan Desa
           Bentek, desa yang terletak di sisi barat Desa Genggelang.
         </p>
         <p className="text-sm md:text-base">
